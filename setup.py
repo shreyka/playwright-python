17069fc067bba79b369f69462fa14eef5ef33436
shreya <shreya.karpoor@gmail.com>
2025-07-07 22:15:25 -0500

    change setup

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import sys
 import zipfile
 from typing import Dict
 
-release_version = "1.52.5"  ## shreya's github release version
+release_version = "1.52.7"  ## shreya's github release version
 driver_version = "1.52.0"
 
 base_wheel_bundles = [
--- test_cdp_resume.py
+++ test_cdp_resume.py
@@ -16,7 +16,7 @@ async def test_cdp_pause_resume():
         
         # === STEP 2: Connect via CDP to localhost:9222 ===
         print('\n🔌 === STEP 2: Connect via CDP to localhost:9222 ===')
-        cdp_endpoint = 'ws://127.0.0.1:9222/devtools/browser/7c99100a-e96a-4251-b13b-de75de41c1d9'
+        cdp_endpoint = 'ws://127.0.0.1:9222/devtools/browser/22a8f5ad-04ce-4867-96f3-3a3dfea9415c'
         
         try:
             cdp_browser = await p.chromium.connect_over_cdp(cdp_endpoint)
@@ -37,10 +37,10 @@ async def test_cdp_pause_resume():
 
             
             # Start first pause in background
-            pause_task = asyncio.create_task(cdp_page.pause(output="test_final_PLEASE_WORK.py", variable=True))
+            pause_task = asyncio.create_task(cdp_page.pause(output="test_final_PLEASE_WORK.py", variable=False))
             
             # Wait a bit, then try to resume
-            await asyncio.sleep(15)
+            await asyncio.sleep(100)
             print('⏭️  Calling first page.resume() via CDP connection...')
             
             try:
@@ -72,10 +72,10 @@ async def test_cdp_pause_resume():
             print('📹 Calling second page.pause() via CDP connection...')
             
             # Start second pause in background
-            pause_task = asyncio.create_task(cdp_page.pause(output="test_final_PLEASE_WORK2.py"))
+            pause_task = asyncio.create_task(cdp_page.pause(output="test_final_PLEASE_WORK2.py", variable=False))
             
             # Wait a bit, then try to resume
-            await asyncio.sleep(5)
+            await asyncio.sleep(120)
             print('⏭️  Calling second page.resume() via CDP connection...')
             
             try:
