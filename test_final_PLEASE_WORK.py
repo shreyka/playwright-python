1410d9c045322072a758eee497adae402d4a585d
shreya <shreya.karpoor@gmail.com>
2025-07-10 01:38:32 -0500

    fix parsing

--- test_final_PLEASE_WORK.py
+++ test_final_PLEASE_WORK.py
@@ -5,8 +5,9 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
-    page.get_by_role("link", name="Login").click()
-    page.get_by_role("textbox", name="Enter your OneID").fill("username")
+    page.locator("div").filter(has_text=re.compile(r"^Promotion$")).nth(1).click()
+    promotion_group = page.get_by_text("Promotion Management")
+    promotion_group.click()
 
     # ---------------------
     context.close()
--- test_final_PLEASE_WORK2.py
+++ test_final_PLEASE_WORK2.py
@@ -5,12 +5,6 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
-    page.locator("[data-test=\"spotlight-search-input\"]").fill("fd")
-    page.locator("[data-test=\"spotlight-search-input\"]").click()
-    serach = "fda"
-    page.locator("[data-test=\"spotlight-search-input\"]").fill(serach)
-    fda = page.locator(".box-border > .mx-auto > div").first
-    fda.click()
 
     # ---------------------
     context.close()
